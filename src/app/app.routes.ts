92c96ea02c7be4e0ea0bfdd47c97bc88d0a7a0ae
Haile <thaile@deloitte.co.uk>
2024-09-30 18:29:35 +0100

    created components and began writing header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskComponent } from './task/task.component';
+import { EventMenuComponent } from './event-menu/event-menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+    { path: 'tasks', component: TaskComponent },
+    { path: 'events', component: EventMenuComponent },
+];
---
+++ src/app/event-menu/event-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-menu.component.html',
+  styleUrl: './event-menu.component.css'
+})
+export class EventMenuComponent {
+
+}
